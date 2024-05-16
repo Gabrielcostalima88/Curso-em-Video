19d957f0d3d937662c803ab0fee874a8f53fde0a
Gabriel Araujo <gabriel.araujo.lima.costa@hotmail.com>
2024-05-16 18:51:26 -0300

    inclusão comentário e ajuste na lógica

--- ex041.py
+++ ex041.py
@@ -4,11 +4,11 @@ ano_nascimento = int(input('Digite o ano de nascimento: '))
 anos =ano_atual - ano_nascimento
 if anos <= 9:
     print('Até 9 anos: MIRIM')
-elif anos > 9 and anos <= 14:
+elif anos <= 14:
     print('Até 14 anos: INFANTIL')
-elif anos > 14 and anos <= 19:
+elif anos <= 19:
     print('Até 19 anos: JUNIOR')
-elif anos == 20:
-    print('Até 20 anos: SÊNIOR')
+elif anos <=25:
+    print('Até 25 anos: SÊNIOR')
 else:
-    print('Acima de 20 anos: MASTER')
+    print('Acima de 25 anos: MASTER')
