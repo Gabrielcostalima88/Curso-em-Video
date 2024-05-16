34d2ac6e391567a4998d3919c616721f82e570a0
Gabriel Araujo <gabriel.araujo.lima.costa@hotmail.com>
2024-05-16 18:24:17 -0300

    inclusão de fatiamento

--- ex037.py
+++ ex037.py
@@ -2,11 +2,10 @@
 n = int(input('Digite um número inteiro:'))
 d = int(input('Você deseja converter o número digitado em qual base? 1-Binário, 2- Octal, 3- Hexadecimal '))
 if d == 1:
-    print('O número {} transformado em binário é {}'.format(n, bin(n)))
+    print('O número {} transformado em binário é {}'.format(n, bin(n)[2:]))
 elif d == 2:
-    print('O número {} transformado em octal é {}'.format(n, oct(n)))
+    print('O número {} transformado em octal é {}'.format(n, oct(n)[2:]))
 elif d == 3:
-    print('O número {} transformado em hexadecimal é {}'.format(n, hex(n)))
+    print('O número {} transformado em hexadecimal é {}'.format(n, hex(n)[2:]))
 else:
     print('Valor inválido')
-
