49d54cb66e6e4e0cd43196249de68834888ae578
Gabriel Araujo <gabriel.araujo.lima.costa@hotmail.com>
2024-05-17 15:56:18 -0300

    contagem regressiva

--- ex039.py
+++ ex039.py
@@ -3,7 +3,9 @@ from datetime import datetime
 ano_atual = datetime.now().year
 ano = int(input('Digite o seu ano de nascimento: '))
 r = (ano_atual - ano)
-if r < 18:
+sexo = str(input('Digite seu sexo: F para feminino e M para masculino'))
+sexo_m = sexo.upper()
+if sexo_m ==  M:
     print('Faltam {} anos para você alistar!'.format(18 - r))
 elif r == 18:
     print('Esse é seu ano de alistamento!')
--- ex040.py
+++ ex040.py
@@ -4,7 +4,7 @@ n2 = float(input('Digite a segunda nota: '))
 m = (n1 + n2) / 2
 if m < 5.0:
     print('Média abaixo de 5.0: REPROVADO!')
-elif m >= 5 and m <= 6.9:
+elif m >= 5 and m < 7:
     print('Média entre 5.0 e 6.9: RECUPERAÇÃO!')
 else:
     print('Média 7.0 ou superior: APROVADO!')
